743accf195cb49ad4a46873ec0c716248144b3f8
Gabriele Falloon <ggfalloon@gmail.com>
2021-03-14 15:06:20 -0500

    added utils folder for api setup

---
+++ src/utils/API.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+export default {
+    getAllEmp: function () {
+        return axios.get('https://randomuser.me/api/');
+    },
+    getByLocation: function (location) {
+        return axios.get();
+    },
+    filterByAge: function () {
+        return axios.get();
+    },
+    getRandomEmp: function () {
+        return axios.get();
+    }
+};
